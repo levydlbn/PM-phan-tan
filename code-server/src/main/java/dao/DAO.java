723dc99deeda9b8880974461a94e129c7853eb29
Ninh <91866873+Hcn16@users.noreply.github.com>
2022-11-30 09:21:35 +0700

    Add files via upload
    Upload phần tạo kết nối với CSDL

--- code-server/src/main/java/dao/DAO.java
+++ code-server/src/main/java/dao/DAO.java
@@ -1,13 +1,29 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.*;
 /**
  *
- * @author 84965
+ * @author Admin
  */
 public class DAO {
-    
+    protected Connection con;
+
+    public DAO() {
+        String jdbcURL = "jdbc:sqlserver://localhost:1433;databaseName=carophantan";
+        String jdbcUsername = "sa";
+        String jdbcPassword = "1234567$"; //please change information to connect to DB
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Connection to database failed");
+        }
+    }
 }
