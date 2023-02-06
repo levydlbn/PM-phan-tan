95a461d48c142139583750b17014555db8155b3a
levy-dl <68326405+levydlbn@users.noreply.github.com>
2023-02-06 13:41:55 +0700

    Update CodeServer.java

--- code-server/src/main/java/controller/CodeServer.java
+++ code-server/src/main/java/controller/CodeServer.java
@@ -1,19 +1,71 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package controller;
 
-
+import view.Admin;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
- * @author 84965
+ * @author Admin
  */
-public class CodeServer {
+public class Server {
+
+    public static volatile ServerThreadBus serverThreadBus;
+    public static Socket socketOfServer;
+    public static int ID_room;
+    public static volatile Admin admin;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ServerSocket listener = null;
+        serverThreadBus = new ServerThreadBus();
+        System.out.println("Server is waiting to accept user...");
+        int clientNumber = 0;
+        ID_room = 100;
+        
+        try {
+            listener = new ServerSocket(7000);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10, // corePoolSize
+                100, // maximumPoolSize
+                10, // thread timeout
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(8) // queueCapacity
+        );
+        admin = new Admin();
+        admin.run();
+        try {
+            while (true) {
+                // Chấp nhận một yêu cầu kết nối từ phía Client.
+                // Đồng thời nhận được một đối tượng Socket tại server.
+                socketOfServer = listener.accept();
+                System.out.println(socketOfServer.getInetAddress().getHostAddress());
+                ServerThread serverThread = new ServerThread(socketOfServer, clientNumber++);
+                serverThreadBus.add(serverThread);
+                System.out.println("Số thread đang chạy là: "+serverThreadBus.getLength());
+                executor.execute(serverThread);  
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                listener.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
